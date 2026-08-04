8b5e0562cad7c34c2601205703c742be9ca84fe4
Shubham Yadav <sky571631@gmail.com>
2026-08-05 02:12:04 +0530

    Add combination calculator in C
    This program calculates the combination of two numbers n and r using the formula nCr = n! / (r! * (n-r)!).

---
+++ functions/calculating combination of number.c
@@ -0,0 +1,27 @@
+//combination calculator
+#include<stdio.h>
+int factorial(int x){
+    int fact=1;
+    for(int i=2;i<=x;i++){
+        fact=fact*i;
+    }
+    return fact;
+
+}
+int main(){
+    int n,r;
+    printf("enter the number n:");
+    scanf("%d",&n);
+    printf("enter the number r:");
+    scanf("%d",&r);
+    int nfact=factorial(n);
+    int rfact=factorial(r);
+    int nrfact=factorial(n-r);
+
+    int ncr=nfact/(rfact*nrfact);
+    printf("%d",ncr);
+    return 0;
+
+
+
+}
